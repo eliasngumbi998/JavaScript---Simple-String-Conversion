d126b2036b1c3a2c36911e00390c476c5ccd17b7
ELIAS NGUMBI <32370330+eliasngumbi998@users.noreply.github.com>
2019-07-26 15:03:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function UpperCase(){
+	var string=document.getElementById('string').innerText;
+	string= string.toUpperCase();
+	document.getElementById('string').innerHTML=string;
+}
+ 
+function LowerCase(){
+	var string=document.getElementById('string').innerText;
+	string= string.toLowerCase();
+	document.getElementById('string').innerHTML=string;
+}
